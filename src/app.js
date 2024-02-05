5ab96032908f9134e726f869b314468faa09f0fe
QZE7 <68819710+QZE7@users.noreply.github.com>
2024-02-05 19:59:46 +0000

    update

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let primero = 0; primero < pronoun.length; primero++) {
+    for (let segundo = 0; segundo < adj.length; segundo++) {
+      for (let tercero = 0; tercero < noun.length; tercero++) {
+        console.log(pronoun[primero] + adj[segundo] + noun[tercero]);
+      }
+    }
+  }
 };
